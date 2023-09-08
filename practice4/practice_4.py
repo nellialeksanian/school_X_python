d3090333dbea1b7e7ed590785691912ef9076825
Nelly Aleksanian <n.aleksanian.sas@gmail.com>
2023-09-08 20:13:52 +0300

    practice 4

--- practice4/practice_4.py
+++ practice4/practice_4.py
@@ -1,16 +1,32 @@
-from import_this import *
+from import_this import generate_race_data
+def sort_racers(RACE_DATA: dict) -> dict:
+    sorted_rd = {}
+    for value in RACE_DATA.values():
+        finplace = value["FinishedPlace"]
+        sorted_rd[finplace] = value
+    return sorted_rd
 
+def print_first_racer (sorted_rd: dict) -> str:
+    winner = f"Имя победителя {sorted_rd[1]['RacerName']}"
+    print(winner + "\n" + "-" * len(winner))
 
+def print_top_racers (sorted_rd: dict) -> str:
+    print("Первые 3 места:\n")
+    for k in range(1, 4):
+        value = sorted_rd[k]
+        print(f"Гонщик на {k} месте\n" +
+              f"\tИмя:{value['RacerName']} \n" +
+              f"\tКоманда:{value['RacerTeam']}\n" +
+              f"\tВремя:{value['FinishedTimeSeconds'] // 3600}:" +
+              f"{value['FinishedTimeSeconds'] % 3600 // 60}:" +
+              f"{value['FinishedTimeSeconds'] % 60} \n"
+              )
 
-# for key, value in RACE_DATA.items():
-#     if key == 1:
-#         RacerName = RACE_DATA
+if __name__ == "__main__":
+    sorted = sort_racers(generate_race_data(10))
+    print_first_racer(sorted)
+    print_top_racers(sorted)
 
-
-print(f"Гонщик на { RACE_DATA.get(1).get('FinishedPlace')} месте")
-print(f'Имя: {RACE_DATA.get(1).get("RacerName")}')
-print(f'Команда: {RACE_DATA.get(1).get("RacerTeam")}')
-print(f'Время: {RACE_DATA.get(1).get("FinishedTimeSeconds")}')
 
 
 
