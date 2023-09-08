f6bfdc20faeedaf19cf8e5b751505f4be2c9860a
Nelly Aleksanian <n.aleksanian.sas@gmail.com>
2023-09-08 22:12:29 +0300

    added

--- practice4/practice_4.py
+++ practice4/practice_4.py
@@ -1,16 +1,20 @@
 from import_this import generate_race_data
-def sort_racers(RACE_DATA: dict) -> dict:
+
+
+def sort_racers(race_data: dict) -> dict:
     sorted_rd: dict = {}
-    for value in RACE_DATA.values():
-        finishplace: int = value["FinishedPlace"]
-        sorted_rd[finishplace] = value
+    for value in race_data.values():
+        finish_place: int = value["FinishedPlace"]
+        sorted_rd[finish_place] = value
     return sorted_rd
 
-def print_first_racer (sorted_rd: dict) -> str:
+
+def print_first_racer(sorted_rd: dict) -> str:
     winner: str = f"Выиграл - {sorted_rd[1]['RacerName']}!!!! Поздравляем!!!"
     print(winner + "\n" + "-" * len(winner))
 
-def print_top_racers (sorted_rd: dict) -> str:
+
+def print_top_racers(sorted_rd: dict) -> str:
     print("Первые 3 места:\n")
     for k in range(1, 4):
         value: dict = sorted_rd[k]
@@ -22,13 +26,8 @@ def print_top_racers (sorted_rd: dict) -> str:
               f"{value['FinishedTimeSeconds'] % 60} (H:M:S) \n"
               )
 
+
 if __name__ == "__main__":
-    sorted: dict = sort_racers(generate_race_data(10))
-    print_first_racer(sorted)
-    print_top_racers(sorted)
-
-
-
-
-
-
+    sorted_dict: dict = sort_racers(generate_race_data(10))
+    print_first_racer(sorted_dict)
+    print_top_racers(sorted_dict)
